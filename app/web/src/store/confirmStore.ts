96d8fb01678fcbf10767f3713dde9bfe55e53dc2
sangminnn <psm0102psm@gmail.com>
2023-11-27 01:27:11 +0900

    imp: confirmStore 내 타입 선언방식 state와 action을 분리하는 방식으로 변경 완료

--- app/web/src/store/confirmStore.ts
+++ app/web/src/store/confirmStore.ts
@@ -6,14 +6,19 @@ export interface OpenConfirmProps {
   cancelText?: React.ReactNode | string;
 }
 
-export interface ConfirmStore {
+interface ConfirmState {
   isOpen: boolean;
   content: React.ReactNode;
+}
+
+interface ConfirmAction {
   confirmText?: React.ReactNode | string;
   cancelText?: React.ReactNode | string;
   openConfirm: ({ content, confirmText, cancelText }: OpenConfirmProps) => void;
   closeConfirm: () => void;
 }
+
+export type ConfirmStore = ConfirmState & ConfirmAction;
 
 /** @TODO 아래와 같이 key, value구조에서 value에 변수할당해주는 구문이 들어가는 템플릿이 맞는지 체크하기 */
 const useConfirmStore = create<ConfirmStore>((set) => ({
