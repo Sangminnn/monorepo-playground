b93988577c5d0778aa8e8767f2e51e969349e210
sangminnn <psm0102psm@gmail.com>
2024-04-24 18:11:04 +0900

    imp: vitest의 메서드를 import없이 사용하기 위한 tsconfig 설정 추가 완료

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), checker({ typescript: true })],
 });
